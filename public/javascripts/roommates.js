fa99f757dbfc9d5745cd4c85714631d8ee9df7cb
arelia <arelia.jones@gmail.com>
2013-12-10 00:10:16 -0600

    add index and js

---
+++ public/javascripts/roommates.js
@@ -0,0 +1,27 @@
+function TodoCtrl($scope) {
+  $scope.todos = [
+    {text:'take out trash', done:true},
+    {text:'clean kitchen', done:false}];
+ //TODO remove sample todos
+
+  $scope.addTodo = function() {
+    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todoText = '';
+  };
+ 
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+ 
+  $scope.archive = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+}
